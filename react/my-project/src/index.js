c71b31946762ddb9440021a20541624698512f0f
protofu <sungjae0512@gmail.com>
2024-06-26 14:03:18 +0900

    learn: react pjt

--- react/my-project/src/index.js
+++ react/my-project/src/index.js
@@ -17,17 +17,6 @@ const onClickLogoutHandle = (e) => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Team /> */}
-    {/* <Clock /> */}
-    {/* <Comment />
-    <NumberList />
-    <FancyBorder />
-    <WelcomeDialog />
-    <Card /> */}
-    {/* <Book /> */}
-    {/* <App /> */}
-    {/* <MainWrap /> */}
-    {/* <BlogMain /> */}
     <ToolBar isLogin={true} onClickLoginHandle={onClickLoginHandle} onClickLogoutHandle={onClickLogoutHandle}/>
     <Greeting isLogin={true}/>
     <MyBtn />
